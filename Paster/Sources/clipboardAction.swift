d1eb6eca49ad5b14e0872ac6a12bdc6537ac63f1
Taeyong Seong <Taeyong Seong>
2020-09-06 13:09:08 +0900

    rename

--- Paster/Sources/clipboardAction.swift
+++ Paster/Sources/clipboardAction.swift
@@ -9,14 +9,10 @@
 import Foundation
 import Cocoa
 
-class stringHandler {
-    
-    private static  let handler = stringHandler()
-    
+class clipboardAction {
     init() {
-        
     }
-    func writeToClipBoard(str : String){
+    func setStr(str : String){
         let pasteboard = NSPasteboard.general
         pasteboard.clearContents()
         if pasteboard.setString(str, forType: NSPasteboard.PasteboardType.string) {
@@ -30,16 +26,5 @@ class stringHandler {
         let pastedBoard = NSPasteboard.general
         guard let str = pastedBoard.string(forType: NSPasteboard.PasteboardType.string) else {return ""}
         return str
-    }
-    func removeCRLF() -> String? {
-        /*
-         return String value which is removed CRLF
-         */
-        guard let str = NSPasteboard.general.string(forType: NSPasteboard.PasteboardType.string)
-            else { return nil}
-        return str.map{(elem : String.Element) -> String in
-            return (elem == "\n" || elem == "\r" ? " " : String(elem))
-        }.reduce(""){ $0 + $1 }
-        
     }
 }
